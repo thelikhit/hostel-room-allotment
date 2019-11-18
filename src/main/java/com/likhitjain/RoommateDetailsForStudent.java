2b0b40f8f261a8f310a5c86ad043468e369da2c7
Likhit Jain <the.likhitjain@gmail.com>
2019-11-18 20:41:28 +0530

    Roommate and warden details for student working

--- src/main/java/com/likhitjain/RoommateDetailsForStudent.java
+++ src/main/java/com/likhitjain/RoommateDetailsForStudent.java
@@ -1,4 +1,55 @@
 package com.likhitjain;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class RoommateDetailsForStudent {
+
+    private static String roommateName1, roommateName2;
+    private static String roommateDept1, roommateDept2;
+    private static String roommateContact1, roommateContact2;
+
+    @FXML
+    public Text name1Text;
+    @FXML
+    public Text dept1Text;
+    @FXML
+    public Text contact1Text;
+    @FXML
+    public Text name2Text;
+    @FXML
+    public Text dept2Text;
+    @FXML
+    public Text contact2Text;
+
+    //if 1 roommate
+    static void setRoommateDetails(String name, String dept, String contact) {
+        roommateName1 = name;
+        roommateDept1 = dept;
+        roommateContact1 = contact;
+    }
+
+    //if 2 roommates
+    static void setRoommateDetails(String name1, String dept1, String contact1, String name2, String dept2, String contact2 ) {
+        roommateName1 = name1;
+        roommateDept1 = dept1;
+        roommateContact1 = contact1;
+        roommateName2 = name2;
+        roommateDept2 = dept2;
+        roommateContact2 = contact2;
+    }
+
+    public void initialize() {
+        if (roommateName1 != null) {
+            name1Text.setText(roommateName1);
+            dept1Text.setText(roommateDept1);
+            contact1Text.setText(roommateContact1);
+        }
+
+        if (roommateName2 != null) {
+            name2Text.setText(roommateName2);
+            dept2Text.setText(roommateDept2);
+            contact2Text.setText(roommateContact2);
+        }
+    }
 }
--- src/main/java/com/likhitjain/StudentHome.java
+++ src/main/java/com/likhitjain/StudentHome.java
@@ -30,19 +30,66 @@ public class StudentHome {
         Connection connection = ConnectionManager.getConnection();
         Statement statement = connection.createStatement();
 
-        String QUERY = "SELECT warden FROM Hostel.Student WHERE usn='" + studentUSN + "';";
+        String QUERY = "SELECT w_name, emp_id, contact FROM Hostel.Warden WHERE emp_id = " +
+                "(SELECT warden FROM Hostel.Block WHERE b_id = " +
+                "(SELECT b_id FROM Hostel.Room WHERE r_id= " +
+                "(SELECT room FROM Hostel.Student WHERE usn='"
+                + studentUSN +"')));\n";
+
+        System.out.println(QUERY);
 
         ResultSet resultSet = statement.executeQuery(QUERY);
         resultSet.next();
 
-        QUERY = "SELECT * FROM Hostel.Warden WHERE emp_id='" + resultSet.getString("warden") + "';";
+        WardenDetailsForStudent.setWardenDetails(resultSet.getString("emp_id"), resultSet.getString("w_name"), resultSet.getString("contact"));
+        App.setRoot("wardenDetailsForStudent");
+    }
+
+    public void onRoommateDetailsButtonClick() throws IOException, SQLException {
+
+        Connection connection = ConnectionManager.getConnection();
+        Statement statement = connection.createStatement();
+
+        String QUERY = "SELECT f_name, l_name, department, mobile_no FROM Hostel.Student WHERE room = " +
+                "(SELECT room FROM Hostel.Student WHERE usn='"
+                +  studentUSN + "') AND usn <> '" + studentUSN + "';";
+
+        System.out.println(QUERY);
+
+        ResultSet resultSet = statement.executeQuery(QUERY);
+
+        resultSet.last();
+        int rowCount = resultSet.getRow();
+        System.out.println("RC: " + rowCount);
+
+        QUERY = "SELECT f_name, l_name, department, mobile_no FROM Hostel.Student WHERE room = " +
+                "(SELECT room FROM Hostel.Student WHERE usn='"
+                +  studentUSN + "') AND usn <> '" + studentUSN + "';";
 
         resultSet = statement.executeQuery(QUERY);
         resultSet.next();
 
-        WardenDetailsForStudent.setWardenDetails(resultSet.getString("emp_id"), resultSet.getString("w_name"), resultSet.getString("contact"));
-        App.setRoot("wardenDetailsForStudents");
+        if (rowCount == 1) {
+            String name = resultSet.getString("f_name") + " " + resultSet.getString("l_name");
+            String contact = resultSet.getString("mobile_no");
+            String dept = resultSet.getString("department");
+            RoommateDetailsForStudent.setRoommateDetails(name, dept, contact);
+        }
+
+        if (rowCount == 2) {
+            String name1 = resultSet.getString("f_name") + " " + resultSet.getString("l_name");
+            String contact1 = resultSet.getString("mobile_no");
+            String dept1 = resultSet.getString("department");
+            resultSet.next();
+            String name2 = resultSet.getString("f_name") + " " + resultSet.getString("l_name");
+            String contact2 = resultSet.getString("mobile_no");
+            String dept2 = resultSet.getString("department");
+            RoommateDetailsForStudent.setRoommateDetails(name1, dept1, contact1, name2, dept2, contact2);
+
+            App.setRoot("roommateDetailsForStudent");
+        }
     }
+
 
     public void onLogout() throws IOException {
         App.setRoot("mainMenu");
